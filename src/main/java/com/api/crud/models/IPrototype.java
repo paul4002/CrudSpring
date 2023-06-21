d56561038943c2c16b329f619ae08e0ac47c49c9
Paúl Valverde <paul-vlvrd@hotmail.com>
2023-06-21 16:33:10 -0400

    Crud inicial

---
+++ src/main/java/com/api/crud/models/IPrototype.java
@@ -0,0 +1,5 @@
+package com.api.crud.models;
+
+public interface IPrototype {
+  public IPrototype clone();
+}
---
+++ src/main/java/com/api/crud/services/UserService.java
@@ -0,0 +1,46 @@
+package com.api.crud.services;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.crud.models.UserModel;
+import com.api.crud.repositories.IUserRepository;
+
+@Service
+public class UserService {
+  
+  @Autowired
+  IUserRepository userRepository;
+
+  public ArrayList<UserModel> getUsers() {
+    return (ArrayList<UserModel>) userRepository.findAll();
+  }
+
+  public UserModel saveUser(UserModel user) {
+    return userRepository.save(user);
+  }
+
+  public Optional<UserModel> getById(Long id) {
+    return userRepository.findById(id);
+  }
+
+  public UserModel updateById(UserModel request,  Long id) {
+    UserModel user = userRepository.findById(id).get();
+    user = (UserModel) request.clone();
+    user.setId(id);
+    userRepository.save(user);
+    return user;
+  }
+
+  public Boolean deleteUser(Long id) {
+    try {
+      userRepository.deleteById(id);
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
+  }
+}
